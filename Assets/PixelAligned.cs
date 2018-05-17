6701878da6d0b15aa4041b5d44db3c02d44f5624
wqferr <wqferr@gmail.com>
2018-05-17 11:47:36 +0000

    Add pixel alignment script

---
+++ Assets/PixelAligned.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelAligned : MonoBehaviour {
+
+	private SpriteRenderer rend;
+
+	void Awake() {
+		rend = GetComponent<SpriteRenderer>();
+	}
+
+	void LateUpdate() {
+		Vector3 newPosition = Vector3.zero;
+		float ppu = rend.sprite.pixelsPerUnit;
+
+        newPosition.x = (Mathf.Round(transform.position.x * ppu) / ppu);
+        newPosition.y = (Mathf.Round(transform.position.y * ppu) / ppu);
+
+        transform.position = newPosition;
+	}
+}
