a215ea79fe75d8173b3237731792368bce8e72cc
Marjorie <marjorieysaito@gmail.com>
2018-05-30 18:17:35 -0300

    Add highlight on spell select targets

--- Assets/Logic/GameManager.cs
+++ Assets/Logic/GameManager.cs
@@ -467,6 +467,9 @@ public class GameManager : MonoBehaviour {
 				selectedUnit, hoveredCell, spellTargets));
 			foreach (HexCell c in spellAOE) {
 				c.highlight = Highlight.IN_AOE;
+				if (c.unit != null) {
+					c.highlight = Highlight.RELEVANT; //change highlight sprite and name
+				}
 			}
 		}
 	}
