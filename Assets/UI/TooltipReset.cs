af533bb9ea1f82c95de012a0c270f1ec7fdc8da6
Marjorie <marjorieysaito@gmail.com>
2018-07-27 13:05:58 -0300

    Lol

---
+++ Assets/UI/TooltipReset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipReset : MonoBehaviour {
+	void OnEnable() {
+		foreach (var tooltip in GameObject.FindObjectsOfType<Tooltip>()) {
+			tooltip.Clear();
+		}
+	}
+}
