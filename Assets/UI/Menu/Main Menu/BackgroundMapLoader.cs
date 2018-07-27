56d56de21f9726e1730f525f733f4021e2ce8ab9
Marjorie <marjorieysaito@gmail.com>
2018-07-27 03:43:22 -0300

    Add background map asset to main menu

---
+++ Assets/UI/Menu/Main Menu/BackgroundMapLoader.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackgroundMapLoader : MonoBehaviour {
+
+	public MapLayout layout;
+
+	void Awake() {
+		SceneManager.sceneLoaded += Loaded;
+		MapLoader.LoadLayout(layout, LoadSceneMode.Additive);
+	}
+
+	void Start() {
+		GameObject[] goArray = SceneManager.GetSceneByName("MapBaseScene").GetRootGameObjects();
+		foreach (GameObject go in goArray) {
+			if (go.name == "Canvas" || go.name == "Menu Canvas" || go.name == "Winner Canvas") {
+				go.SetActive(false);
+			}
+		}
+	}
+
+	void Loaded(Scene scene, LoadSceneMode mode) {
+		SceneManager.sceneLoaded -= Loaded;
+		//Invoke("OnLoad", 0.1f);
+	}
+
+	void OnLoad() {
+	}
+
+}
