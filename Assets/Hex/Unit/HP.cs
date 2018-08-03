a30a680aedebc44a3c3e5ebd3bee58077c31af14
wqferr <wqferr@gmail.com>
2018-08-03 00:50:31 -0300

    Add damage display to units and things that take damage

--- Assets/Hex/Unit/HP.cs
+++ Assets/Hex/Unit/HP.cs
@@ -7,26 +7,34 @@ using UnityEngine.UI;
 [RequireComponent(typeof(HexUnit))]
 public class HP : MonoBehaviour {
 	public GameObject prefabHealthBar;
+	public GameObject prefabDamageDisplay;
 	public float healthBarHeight = 0.5f;
+	public float damageDisplayHeight = 0;
+	public float damageDisplayDuration = 1.0f;
 
 	private GameObject healthBar;
 	private GameObject healthpoint;
+	private GameObject damageDisplay;
+	private TMPro.TMP_Text damageDisplayText;
 	private Transform healthBarFill;
 	private Text hpText;
 
 	public int max;
-	private int _current;
-	public int current {
+	private int? _current;
+	public int? current {
 		get {
 			return _current;
 		}
 		set {
-			_current = Mathf.Max(0, Mathf.Min(value, max));
+			var prev = _current;
+			_current = (int) Mathf.Max(0, Mathf.Min((float) value, max));
 			Vector3 scale = healthBarFill.localScale;
 			scale.x = (float) _current / max;
 			healthBarFill.localScale = scale;
 			if (_current == 0) {
 				OnReachZero();
+			} else if (prev != null) {
+				DisplayDamage(prev, current);
 			}
 		}
 	}
@@ -35,14 +43,38 @@ public class HP : MonoBehaviour {
 
 	void Awake() {
 		healthBar = Instantiate(prefabHealthBar, transform, false);
+		damageDisplay = Instantiate(prefabDamageDisplay, transform, false);
 		healthBarFill = healthBar.transform.Find("Fill");
 		current = max;
 		healthpoint = healthBar.transform.Find("Healthpoint").gameObject;
 		Transform textTr = healthpoint.transform.Find("Text");
 		hpText = textTr.GetComponent<Text>();
+		textTr = damageDisplay.transform.Find("Text");
+		damageDisplayText = textTr.GetComponent<TMPro.TMP_Text>();
 		Vector3 pos = healthBar.transform.localPosition;
 		pos.y = healthBarHeight;
 		healthBar.transform.localPosition = pos;
+		pos = damageDisplay.transform.localPosition;
+		pos.y = damageDisplayHeight;
+		damageDisplay.transform.localPosition = pos;
+
+		damageDisplay.SetActive(false);
+	}
+
+	public void DisplayDamage(int? prev, int? cur) {
+		if (cur < prev) {
+			damageDisplayText.text = (cur - prev).ToString();
+		} else {
+			damageDisplayText.text = string.Format("+{0}", cur - prev);
+		}
+		damageDisplay.SetActive(true);
+		// reset invoke timer
+		CancelInvoke("DisableDamageDisplay");
+		Invoke("DisableDamageDisplay", damageDisplayDuration);
+	}
+
+	private void DisableDamageDisplay(){
+		damageDisplay.SetActive(false);
 	}
 
 	public void UpdateNumberDisplay() {
